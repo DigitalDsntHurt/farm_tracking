d4754c86400ecc413c5534ecd5c4555b4aa7e452
N Smith <digitaldoesnthurt@gmail.com>
2018-01-16 19:19:19 -0800

    set homepage to seed_flats#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :seed_flats
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'seed_flats#index'
 end
