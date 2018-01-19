9c996f7c9fc9fa26534f50a3275cb648043ba19c
N Smith <digitaldoesnthurt@gmail.com>
2018-01-19 09:38:56 -0800

    sort order on seef flats index page

--- app/controllers/seed_flats_controller.rb
+++ app/controllers/seed_flats_controller.rb
@@ -4,7 +4,7 @@ class SeedFlatsController < ApplicationController
   # GET /seed_flats
   # GET /seed_flats.json
   def index
-    @seed_flats = SeedFlat.all.order(:started_date)
+    @seed_flats = SeedFlat.all.order(:created_at)
   end
 
   # GET /seed_flats/1
