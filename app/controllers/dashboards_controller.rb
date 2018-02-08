30453aec7ecf371a02bfade0f090b8e189943f0b
N Smith <digitaldoesnthurt@gmail.com>
2018-02-08 15:37:26 -0800

    debugs

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,7 +8,7 @@ class DashboardsController < ApplicationController
   def pipeline
     @propagation_shelf = SeedFlat.where(:date_of_first_transplant => nil)
     @sue_shelf = SeedFlat.where.not(:date_of_first_transplant => nil).where(:date_of_second_transplant => nil).where(:date_of_third_transplant => nil).where(:harvested_on => nil)
-    @david_shelf = SeedFlat.where.not(:date_of_second_transplant => nil).where(:date_of_third_transplant => nil)#.where(:harvested_on => nil)
+    @david_shelf = SeedFlat.where.not(:date_of_second_transplant => nil).where(:date_of_third_transplant => nil).where(:harvested_on => nil)
     @live_storage_shelf = SeedFlat.where.not(:date_of_third_transplant => nil)#.where(:harvested_on => nil)
   end
 
