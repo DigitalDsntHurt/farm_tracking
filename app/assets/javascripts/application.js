7bda351162ed51e6af59fbbd0189cee3c7ad587f
David Lindsey <david.a.lindsey@gmail.com>
2018-02-17 17:28:19 -0500

    changed pipeline view and started on seed flat index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
