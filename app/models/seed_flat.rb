ee394f28cf2e83eb3e3828df1eef6e8c510df061
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2018-10-09 01:17:49 -0700

    current crop summaries in back of envelope dash, sets current system id to nil on harvest

--- app/models/seed_flat.rb
+++ app/models/seed_flat.rb
@@ -4,15 +4,21 @@ class SeedFlat < ApplicationRecord
 
 	before_create :set_current_system_id, :upcase_flat_id, :calculate_harvest_week, :convert_oz_to_lbs
 	after_create :set_date_of_first_flat_sew_on_seed_treatment, :set_date_of_last_flat_sew_on_seed_treatment, :set_destination_flat_ids_on_seed_treatment, :create_seed_flat_update
-	before_update :move_flat_id_to_former_flat_id_on_harvest_or_kill, :kill_flat_id_on_harvest, :update_harvest_date_on_harvest, :calculate_harvest_week, :convert_oz_to_lbs, :set_days_to_harvest_from_sew, :set_days_to_harvest_from_soak
+	before_update :move_flat_id_to_former_flat_id_on_harvest_or_kill, :kill_flat_id_on_harvest, :remove_current_system_id_on_harvest, :update_harvest_date_on_harvest, :calculate_harvest_week, :convert_oz_to_lbs, :set_days_to_harvest_from_sew, :set_days_to_harvest_from_soak
 	#after_update :move_flat_id_to_harvest_notes_on_harvest
 	validates_uniqueness_of :flat_id
 
 	private
 
-	def set_current_system_id
+	def set_current_system_id_to_propagation
 		current_system = Room.where(id: self.room_id)[0].systems.where(system_name: "propagation")
 		self.current_system_id = current_system[0].id
+	end
+
+	def remove_current_system_id_on_harvest
+		unless harvest_weight_oz == nil
+			self.current_system_id = nil
+		end
 	end
 
 	def upcase_flat_id
