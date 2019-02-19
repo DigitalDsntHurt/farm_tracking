4b9552524452931abf74b077f9ccadaf69027a05
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2019-02-19 10:44:40 -0800

    sdfs

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,6 +2,6 @@ require 'rufus-scheduler'
 
 s = Rufus::Scheduler.new
 
-s.every '30m'#, :first_at => Time.now + 10 do 
+s.every '30m' do #, :first_at => Time.now + 10 
 	Scheduled.create(date: Date.today, verb: "sew", crop: "radish", variety: "purple sango", customer: "Liholiho Yacht Club", order: "111", completed_on: nil)
 end
