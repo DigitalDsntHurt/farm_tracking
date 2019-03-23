18af7dcb972a470579d310145027ae401f581fd0
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2019-03-23 14:06:55 -0700

    deleted scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,6 +2,6 @@ require 'rufus-scheduler'
 
 s = Rufus::Scheduler.new
 
-s.every '30m' do #, :first_at => Time.now + 10 
-	Scheduled.create(date: Date.today, verb: "sew", crop: "radish", variety: "purple sango", customer: "Liholiho Yacht Club", order: "111", completed_on: nil)
-end
+#s.every '30m' do #, :first_at => Time.now + 10 
+#	Scheduled.create(date: Date.today, verb: "sew", crop: "radish", variety: "purple sango", customer: "Liholiho Yacht Club", order: "111", completed_on: nil)
+#end
