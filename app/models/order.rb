af28a063594e5c8054085acdd163b4368cc0ec85
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2019-07-15 08:03:31 -0700

    redundant orders model method to capitalize day_of_week field

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
 
-	before_create :set_standing_order_to_false
+	before_create :set_standing_order_to_false :capitalize_day_of_week
 
 	private
 
@@ -8,5 +8,9 @@ class Order < ApplicationRecord
 		unless self.standing_order == true
 			self.standing_order = false
 		end
+	end
+
+	def capitalize_day_of_week
+		self.day_of_week = self.day_of_week.capitalize unless self.day_of_week == nil
 	end
 end
